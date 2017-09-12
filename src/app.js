d96bc54df5639070c0e3e95e92f70aa1b8eeaf56
RyanLuu <rl65536@gmail.com>
2017-09-12 19:14:14 -0400

    Moles slider

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+function updateMoles(moles) {
+    if (moles > window.pJSDom[0].pJS.particles.array.length) {
+        window.pJSDom[0].pJS.fn.modes.pushParticles(moles - window.pJSDom[0].pJS.particles.array.length)
+    } else if (moles < window.pJSDom[0].pJS.particles.array.length) {
+        window.pJSDom[0].pJS.fn.modes.removeParticles(window.pJSDom[0].pJS.particles.array.length - moles)
+    }
+}
