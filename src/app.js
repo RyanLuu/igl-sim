b81857a536ae36d3e7e33fc3789c7cc634615239
RyanLuu <rl65536@gmail.com>
2017-09-12 20:27:28 -0400

    Particle highlight fix

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
-  // Highlight particle
-  window.pJSDom[0].pJS.particles.array[0].color={value:"#FF0000",rgb:{r:255,g:0,b:0}}
+  highlightParticle();
 });
 
 function updateMoles(moles) {
@@ -10,4 +9,12 @@ function updateMoles(moles) {
     } else if (moles < window.pJSDom[0].pJS.particles.array.length) {
         window.pJSDom[0].pJS.fn.modes.removeParticles(window.pJSDom[0].pJS.particles.array.length - moles)
     }
+
+    if (moles > 0) {
+        highlightParticle();
+    }
+}
+
+function highlightParticle() {
+    window.pJSDom[0].pJS.particles.array[0].color={value:"#FF0000",rgb:{r:255,g:0,b:0}}
 }
