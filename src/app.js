1eb0e2fbcef1fd0792944a434fdadceea26c73bb
RyanLuu <rl65536@gmail.com>
2017-09-13 16:47:31 -0400

    Added volume control

--- src/app.js
+++ src/app.js
@@ -15,6 +15,10 @@ function updateMoles(moles) {
     }
 }
 
+function updateVolume(volume) {
+    window.pJSDom[0].pJS.canvas.h0 = volume;
+}
+
 function highlightParticle() {
     window.pJSDom[0].pJS.particles.array[0].color={value:"#FF0000",rgb:{r:255,g:0,b:0}}
 }
