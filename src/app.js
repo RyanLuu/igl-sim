16d636fe5fc7c7297f3f2c8f5a5bbabdcc345d11
RyanLuu <rl65536@gmail.com>
2017-09-12 20:08:37 -0400

    Highlight particle 0

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
+  // Highlight particle
+  window.pJSDom[0].pJS.particles.array[0].color={value:"#FF0000",rgb:{r:255,g:0,b:0}}
 });
 
 function updateMoles(moles) {
