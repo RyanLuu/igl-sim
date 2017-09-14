05a142768ae972181c142a77c3857a2d3a66b5dd
RyanLuu <rl65536@gmail.com>
2017-09-13 20:48:50 -0400

    Temperature slider

--- src/app.js
+++ src/app.js
@@ -26,6 +26,10 @@ function updateVolume(volume) {
     window.pJSDom[0].pJS.canvas.h0 = new_h0;
 }
 
+function updateTemperature(temperature) {
+    window.pJSDom[0].pJS.particles.move.speed = temperature;
+}
+
 function highlightParticle() {
     window.pJSDom[0].pJS.particles.array[0].color={value:"#FF0000",rgb:{r:255,g:0,b:0}}
 }
