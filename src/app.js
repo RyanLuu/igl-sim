2eee8947952fb8ff0cb47876536ae00b427da800
RyanLuu <rl65536@gmail.com>
2017-09-18 00:41:19 -0400

    quick n dirty mean filter

--- src/app.js
+++ src/app.js
@@ -15,13 +15,15 @@ particlesJS.load('particles-js', 'assets/particles.json', function() {
       context.clearRect(0, 0, width, height);
       var max_pressure = Math.max.apply(null, pressure_history);
       context.beginPath();
+      var y0 = height * (1 - ((pressure_history[0] + pressure_history[1]) / 2) / max_pressure);
       context.moveTo(0, y0);
-      var y0 = height * (1 - pressure_history[0] / max_pressure);
-      for (var i = 1; i < pressure_history.length; i++) {
+      for (var i = 1; i < pressure_history.length - 1; i++) {
           var x1 = width * (i / pressure_history.length);
-          var y1 = height * (1 - pressure_history[i] / max_pressure);
+          var y1 = height * (1 - ((pressure_history[i-1] + pressure_history[i] + pressure_history[i+1]) / 3) / max_pressure);
           context.lineTo(x1, y1);
       }
+      var y2 = height * (1 - ((pressure_history[pressure_history.length-2] + pressure_history[pressure_history.length-1]) / 2) / max_pressure);
+      context.lineTo(width, y2);
       context.stroke();
       requestAnimationFrame(update);
   }
@@ -58,7 +60,7 @@ function highlightParticle() {
     window.pJSDom[0].pJS.particles.array[0].color={value:"#FF0000",rgb:{r:255,g:0,b:0}}
 }
 
-var pressure_history = [];
+var pressure_history = Array.apply(null, Array(50)).map(Number.prototype.valueOf, 0);
 var t_last = Date.now();
 
 function pollPressure() {
@@ -68,9 +70,7 @@ function pollPressure() {
     console.log(pressure);
     if (!isNaN(pressure)) {
         pressure_history.push(pressure);
-        if (pressure_history.length > 50) {
-            pressure_history.splice(0, 1);
-        }
+        pressure_history.splice(0, 1);
     }
     window.pJSDom[0].pJS.pressure = 0;
     t_last = now;
